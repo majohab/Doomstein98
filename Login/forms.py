7a0b0fec3cbf4594a98d4e8e2024a443d379f3d7
Jonas.Haberstroh <Jonas.Haberstroh@dhbw-engineering.de>
2022-04-05 15:44:40 +0200

    Changed form label display.

--- Login/forms.py
+++ Login/forms.py
@@ -6,7 +6,6 @@ class RegistrationForm(UserCreationForm):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.fields['user_name'].widget.attrs.update({
-            'id': "text18",
             'maxlength': "100",
             'type': "text",
         }) 
@@ -14,25 +13,22 @@ class RegistrationForm(UserCreationForm):
         self.fields['user_name'].help_text = 'Required. 100 characters or fewer. Letters, digits and @/./+/-/_ only.'
         email = forms.EmailField(max_length=100, help_text="Enter a valid email adress.")
         self.fields['email'].widget.attrs.update({
-            'id': "text18",
             'maxlength':"100",
             'type':"email",
         })
         self.fields['email'].label = 'EMAIL'
         self.fields['email'].help_text = 'Enter a valid email adress.'
         self.fields['password1'].widget.attrs.update({
-            'id': "text18",
             'maxlength':"254",
             'type':"password",
         })
-        self.fields['password1'].label = 'Password'
+        self.fields['password1'].label = 'PASSWORD'
         self.fields['password1'].help_text = 'Enter a strong password.'
         self.fields['password2'].widget.attrs.update({
-            'id': "text18",
             'maxlength':"254",
             'type':"password",
         })
-        self.fields['password2'].label = 'Confirm Password'
+        self.fields['password2'].label = 'CONFIRM PASSWORD'
         self.fields['password2'].help_text = 'Repeat your password.'
 
     class Meta:
@@ -54,13 +50,11 @@ class UserLoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.fields['username'].widget.attrs.update({
-            'id': "text18",
             'type': "text",
         }) 
         self.fields['username'].label = 'EMAIL'
 
         self.fields['password'].widget.attrs.update({
-            'id': "text18",
             'type': "text",
         }) 
         self.fields['password'].label = 'PASSWORD'
