97ca97469efb475ed20fa45c023da37048b1ebbb
inf20079 <inf20079@lehre.dhbw-stuttgart.de>
2022-04-14 15:52:08 +0200

    Integrated impressum, privacy and new menu. Changed URL config.

--- Mainmenu/urls.py
+++ Mainmenu/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from . import  views
 
 urlpatterns = [
-    path('', views.menu, name="menu"),
+    path('', views.play, name="play"),
+    path('menu/', views.menu, name="menu"),
+    path('impressum/', views.impressum, name="impressum"),
 ]
--- Mainmenu/views.py
+++ Mainmenu/views.py
@@ -3,4 +3,35 @@ from django.shortcuts import render
 # Create your views here.
 
 def menu(request):
-    return render(request, 'lobby.html', {})
+    """Renders HTML document and sends the response.
+
+    Args:
+        request (GET): Get menu page.
+
+    Returns:
+        HTTP Response: Menu page
+    """
+    return render(request, 'menu.html', {})
+
+def play(request):
+    """Renders HTML document and sends the response.
+
+    Args:
+        request (GET): Get start page.
+
+    Returns:
+        HTTP Response: Start page
+    """
+    return render(request, 'play.html', {})
+
+    
+def impressum(request):
+    """Renders HTML document and sends the response.
+
+    Args:
+        request (GET): Get impressum page.
+
+    Returns:
+        HTTP Response: Impressum page
+    """
+    return render(request, 'impressum.html', {})
